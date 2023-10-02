d63a6a02bc378a5b66f2ce44fd44ae996febb8fb
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2023-10-03 01:15:42 +0200

    Set up navbar icon

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Image from 'next/image'
 import Link from 'next/link'
+import Logo from 'app/components/noun-delete-1394852.svg'
 
 export default function Navbar() {
     return (
         <nav>
+            <Image
+                src={Logo} 
+                alt="logo" 
+                width={70}
+                quality={100}
+                // placeholder='blur'
+                />
             <h1>Helpdesk</h1>
             <Link href={"/"}>Dashboard</Link>
             <Link href={"/tickets"}>Tickets</Link>
